4a8549df689f22b6ac39ddcee5ec67f18fed056b
Allan TARCY <tarcy.allan@orange.fr>
2023-09-16 14:00:59 -0400

    Etape 1 v2 Allan

---
+++ exo_s2_clinique/src/main/java/org/example/clinique/CliniqueVideException.java
@@ -0,0 +1,7 @@
+package org.example.clinique;
+
+public class CliniqueVideException extends IndexOutOfBoundsException{
+    public CliniqueVideException(String messageErreur) {
+        super(messageErreur);
+    }
+}
