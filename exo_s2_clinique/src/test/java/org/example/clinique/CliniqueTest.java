2a3f6907095597d1f96fbd75a1f670a3fc35ede3
Allan TARCY <tarcy.allan@orange.fr>
2023-09-16 15:09:03 -0400

    Etape 2 v1 Allan

--- exo_s2_clinique/src/test/java/org/example/clinique/CliniqueTest.java
+++ exo_s2_clinique/src/test/java/org/example/clinique/CliniqueTest.java
@@ -4,7 +4,6 @@ package org.example.clinique;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.example.triage.type.TriageType;
 import org.example.visiblesymptom.type.VisibleSymptom;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -14,15 +13,16 @@ class CliniqueTest {
 
     private Clinique clinique;
 
-    @BeforeEach
-    void setUp() {
-        TriageType medecinTriageType = TriageType.FIFO;
-        TriageType radiologieTriageType = TriageType.FIFO;
+    private void setUp() {
+        clinique = new Clinique(TriageType.FIFO, TriageType.FIFO);
+    }
+
+    private void setUp(TriageType medecinTriageType, TriageType radiologieTriageType) {
         clinique = new Clinique(medecinTriageType, radiologieTriageType);
     }
 
     private void exceptionListeMedecinVide() {
-        clinique.obtenirProchainPatientPourMedecin();
+        clinique.obtenirProchainPatientFileMedecin();
     }
 
     private void exceptionListeRadiologieVide() {
@@ -33,8 +33,13 @@ class CliniqueTest {
         clinique.triagePatient(name, gravity, visibleSymptom);
     }
 
+    private void unPatientDansFileMedecinEtRadiologie(String name, int gravity, VisibleSymptom visibleSymptom) {
+        clinique.triagePatient(name, gravity, visibleSymptom);
+    }
+
     @Test
     public void quandInstanciation_devraitRetournerTriageType() {
+        setUp();
 
         assertEquals(TriageType.FIFO, clinique.obtenirMedecinTriageType());
         assertEquals(TriageType.FIFO, clinique.obtenirRadiologieTriageType());
@@ -42,6 +47,7 @@ class CliniqueTest {
 
     @Test
     public void quandInstanciation_devraitEtreVide() {
+        setUp();
 
         assertTrue(clinique.listeMedecinEstVide());
         assertTrue(clinique.listeRadiologieEstVide());
@@ -49,6 +55,7 @@ class CliniqueTest {
 
     @Test
     public void cliniqueVide_quandObtenirProchainPatient_devraitRetournerException() {
+        setUp();
 
         ThrowingCallable exceptionListeMedecinVide = this::exceptionListeMedecinVide;
         ThrowingCallable exceptionListeRadiologieVide = this::exceptionListeRadiologieVide;
@@ -60,31 +67,54 @@ class CliniqueTest {
 
     @Test
     public void cliniqueVide_quandPatientArriveAvecMigraine_devraitRemplirFileMedecinUniquement() {
+        setUp();
 
         clinique.triagePatient("Bob", 1, VisibleSymptom.MIGRAINE);
 
-        assertThat("Bob").isEqualTo(clinique.obtenirProchainPatientPourMedecin());
+        assertThat("Bob").isEqualTo(clinique.obtenirProchainPatientFileMedecin());
         assertTrue(clinique.listeRadiologieEstVide());
     }
 
     @Test
     public void cliniqueVide_quandPatientArriveAvecSymptomesRadiologie_devraitEtreAjouteAFileMedecinEtRadiologie() {
+        setUp();
 
         clinique.triagePatient("Bob", 5, VisibleSymptom.BROKEN_BONE);
 
-        assertThat("Bob").isEqualTo(clinique.obtenirProchainPatientPourMedecin());
+        assertThat("Bob").isEqualTo(clinique.obtenirProchainPatientFileMedecin());
         assertThat("Bob").isEqualTo(clinique.obtenirProchainPatientPourRadiologie());
     }
 
     @Test
-    public void unPatientFileMedecinEtRadiologie_quandUnPatientArriveAvecFLU_devraitEtreDeuxiemeDansFileMedecinUniquement() {
-
+    public void unPatientFileMedecinFIFO_quandUnPatientArriveAvecFLU_devraitEtreDeuxiemeDansFileMedecinUniquement() {
+        setUp();
         unPatientDansFileMedecin("Jean", 1, VisibleSymptom.BROKEN_BONE);
 
         clinique.triagePatient("Julie", 1, VisibleSymptom.FLU);
 
         assertThat("Julie").isEqualTo(clinique.obtenirDernierPatientFileMedecin());
         assertThat("Julie").isNotEqualTo(clinique.obtenirDernierPatientFileRadiologie());
+    }
+
+    @Test
+    public void unPatientFileMedecinGRAVITY_quandPatientArriveAvecFLUGravity7_devraitEtrePremierServi() {
+        setUp(TriageType.GRAVITY, TriageType.FIFO);
+        unPatientDansFileMedecin("Paul", 1, VisibleSymptom.COLD);
+
+        clinique.triagePatient("Clara", 7, VisibleSymptom.FLU);
+
+        assertThat("Clara").isEqualTo(clinique.obtenirProchainPatientFileMedecin());
+        assertThat("Paul").isEqualTo(clinique.obtenirProchainPatientFileMedecin());
+    }
+
+    @Test
+    public void unPatientFileMedecinGRAVITYetRadiologieFIFO_quandPatientArriveAvecBROKEN_BONEGravity7_devraitEtrePremierServi() {
+        setUp(TriageType.GRAVITY, TriageType.FIFO);
+        unPatientDansFileMedecinEtRadiologie("Marie", 4, VisibleSymptom.SPRAIN);
+
+        clinique.triagePatient("Jacques", 7, VisibleSymptom.BROKEN_BONE);
+
+        assertThat("Jacques").isEqualTo(clinique.obtenirDernierPatientFileRadiologie());
     }
 
 
